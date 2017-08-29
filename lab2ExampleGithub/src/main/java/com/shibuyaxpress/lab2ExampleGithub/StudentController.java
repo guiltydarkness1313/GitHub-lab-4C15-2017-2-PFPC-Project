deae95e87b83c3211013e5033c8f88da4bd9397d
Paul Frank <paul.frankpc@gmail.com>
2017-08-29 09:12:47 -0500

    nuevo commit y push

---
+++ lab2ExampleGithub/src/main/java/com/shibuyaxpress/lab2ExampleGithub/StudentController.java
@@ -0,0 +1,13 @@
+package com.shibuyaxpress.lab2ExampleGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello dlroW!" );
+    }
+}
